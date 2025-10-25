364f64b0b67d92fb23e5c179149edd26d49227eb
alecomte <al.lecomte@hotmail.com>
2025-10-25 21:43:05 +0200

    post project

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,34 +1,47 @@
 const { default: mongoose } = require("mongoose");
 const Project = require("../models/project.model");
 const User = require("../models/user.model");
-const {allExistByIds} = require('../utils/dbCheck.utils')
+const { allExistByIds } = require("../utils/dbCheck.utils");
 
 exports.createProject = async (req, res) => {
-    const {members} = req.body;
+  try {
+    const { members } = req.body;
     const allMembersExist = await allExistByIds(User, members);
 
     if (!allMembersExist) {
-        return res.status(400).json({ error: "One or several members do not exist" });
+      return res
+        .status(400)
+        .json({ error: "One or several members do not exist" });
     }
 
-    console.log('Creating project with data:', req.body);
-    res.status(200).json({ message: 'ok' });
+    const project = new Project({
+      ...req.body,
+      owner: req.user.id,
+    });
+
+    console.log("Creating project :", project);
+    res.status(201).json(await project.save());
+  } catch (error) {
+    console.log("Erreur dans POST /projects :", error);
+    return res.status(500).json({ error });
+  }
 };
 
 exports.getProjects = async (req, res) => {
-    console.log('Fetching all projects');
-    res.status(200).json({ message: 'ok' });
+    
+  console.log("Fetching all projects");
+  res.status(200).json({ message: "ok" });
 };
 
 exports.getProjectById = async (req, res) => {
-    console.log('Fetching project with ID:', req.params.id);
-    res.status(200).json({ message: 'ok' });
+  console.log("Fetching project with ID:", req.params.id);
+  res.status(200).json({ message: "ok" });
 };
 
-exports.updateProject = async (req, res, next) => {}
+exports.updateProject = async (req, res, next) => {};
 
-exports.deleteProject = async (req, res, next) => {}
+exports.deleteProject = async (req, res, next) => {};
 
-exports.addOneMemberToOneProject = async (req, res, next) => {}
+exports.addOneMemberToOneProject = async (req, res, next) => {};
 
-exports.deleteOneMemberFromOneProject = async (req, res, next) => {}
+exports.deleteOneMemberFromOneProject = async (req, res, next) => {};
