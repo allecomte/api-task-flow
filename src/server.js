e3ac3cf5b9bbd4e942775b74d3726c81149f2310
alecomte <al.lecomte@hotmail.com>
2025-11-10 11:26:32 +0100

    vercel deployment

--- src/server.js
+++ src/server.js
@@ -17,4 +17,5 @@ server.on('listening', () => {
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
   console.log('Listening on ' + bind);
 });
-server.listen(port);
+// server.listen(port);
+module.exports = server;
